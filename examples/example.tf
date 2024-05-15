9f3e8eb8c9f10935836124ffd13f899575c2f3d6
Thulasiraj Komminar <39799163+thulasirajkomminar@users.noreply.github.com>
2024-05-15 17:11:24 +0200

    patch: made user profile resource only for IAM (#7)

--- examples/example.tf
+++ examples/example.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.62.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
